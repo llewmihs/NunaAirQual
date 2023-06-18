3b214a93c505c9116af478640818f3a2b85275f8
llewmihs <joeshimwell@gmail.com>
2023-06-18 10:42:28 -0600

    Update pm_sensor.py
    Update to included moving line

--- pm_sensor.py
+++ pm_sensor.py
@@ -5,6 +5,16 @@ from pms5003 import PMS5003
 import ST7735
 from PIL import Image, ImageDraw, ImageFont
 
+# Additional global variables for managing the display
+last_readings = (0, 0, 0)
+display_update_interval = 0.1  # 10 fps
+time_last_update = time.time()
+
+# Global variables for managing the pulley line animation
+pulley_line_x = 0
+pulley_line_dir = 1
+pulley_line_width = 20
+
 # Display setup
 WIDTH, HEIGHT = 160, 80
 disp = ST7735.ST7735(
@@ -21,7 +31,18 @@ font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"
 colour = (255, 255, 255)
 
 # Create a function to update display
+# Updated display function with pulley line animation
 def update_display(pm_1, pm_2_5, pm_10):
+    global pulley_line_x, pulley_line_dir, pulley_line_width
+    global last_readings, time_last_update, display_update_interval
+
+    now = time.time()
+    if now - time_last_update >= display_update_interval:
+        # Only update the display according to the desired interval (10 fps)
+        time_last_update = now
+        last_readings = (pm_1, pm_2_5, pm_10)
+
+    pm_1, pm_2_5, pm_10 = last_readings
     img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
     draw = ImageDraw.Draw(img)
     rect_colour = (0, 180, 180)
@@ -30,8 +51,20 @@ def update_display(pm_1, pm_2_5, pm_10):
     text = f"PM 1: {pm_1} µg/m³\nPM 2.5: {pm_2_5} µg/m³\nPM 10: {pm_10} µg/m³"
     draw.text((0, 0), text, font=font, fill=colour)
 
-    disp.display(img)
+    # Draw the pulley line
+    pulley_line_y = HEIGHT - 10
+    draw.rectangle((pulley_line_x, pulley_line_y,
+                    pulley_line_x + pulley_line_width, pulley_line_y + 5),
+                   fill=(255, 255, 255))
 
+    # Update the pulley line position
+    if pulley_line_x + pulley_line_dir * 2 >= WIDTH - pulley_line_width:
+        pulley_line_dir = -1
+    elif pulley_line_x + pulley_line_dir * 2 <= 0:
+        pulley_line_dir = 1
+    pulley_line_x += pulley_line_dir * 2
+
+    disp.display(img)
 # Rest of the code is unchanged
 file_number = 0
 csv_file_path = f'/mnt/usb_share/pm_data_{file_number}.csv'
@@ -53,7 +86,6 @@ while True:
     pm_10 = readings.pm_ug_per_m3(10)
 
     print(pm_1, pm_2_5, pm_10)
-    update_display(pm_1, pm_2_5, pm_10)
 
     timestamp = round(time.time())
 
@@ -61,4 +93,7 @@ while True:
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow([timestamp, pm_1, pm_2_5, pm_10])
 
-    time.sleep(1)
+    # Update the display more frequently
+    while time.time() - timestamp < 5:
+        update_display(pm_1, pm_2_5, pm_10)
+        time.sleep(display_update_interval)
